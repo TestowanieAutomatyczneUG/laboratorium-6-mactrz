a8fa85e134d6eac0332cca277b3c9ec04395c748
Maciej_Trzasalski <mtrzsalski@gmail.com>
2020-11-09 17:39:53 +0100

    test2green

--- zad2/src/app.py
+++ zad2/src/app.py
@@ -1,6 +1,24 @@
 class Pass():
     def ValidPassword(self, passw):
-        if passw == "Password1$":
+        letters = False
+        special = False
+        bigletter = False
+        isnumber = False
+        number = 0
+        for i in passw:
+            if i.isupper():
+                bigletter = True
+            if i.isalpha():
+                number += 1
+            if i.isnumeric():
+                isnumber = True
+            if not i.isalnum():
+                special = True
+        if number >= 8:
+            letters = True
+
+        print(letters,special,bigletter,isnumber)
+        if letters and special and bigletter and isnumber:
             return True
         else:
             return False
--- zad2/tests/testsapp.py
+++ zad2/tests/testsapp.py
@@ -6,4 +6,5 @@ class PasswordTest(unittest.TestCase):
     def test_1_true(self):
         self.assertEqual(paass.ValidPassword("Password1$"), True)
     def test_2_true(self):
-        self.assertEqual(paass.ValidPassword("maPasswerd**"), True)
+        self.assertEqual(paass.ValidPassword("maPasswe2rd**"), True)
+    
