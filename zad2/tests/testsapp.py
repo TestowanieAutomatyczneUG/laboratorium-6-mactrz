554ffbc5f0e542c89cae21cd54fc3caaade212aa
Maciej_Trzasalski <mtrzsalski@gmail.com>
2020-11-09 17:41:15 +0100

    test3green

--- zad2/tests/testsapp.py
+++ zad2/tests/testsapp.py
@@ -7,4 +7,5 @@ class PasswordTest(unittest.TestCase):
         self.assertEqual(paass.ValidPassword("Password1$"), True)
     def test_2_true(self):
         self.assertEqual(paass.ValidPassword("maPasswe2rd**"), True)
-    
+    def test_3_false_no_special(self):
+        self.assertEqual(paass.ValidPassword("PppPaAcc2dasx"), False)
