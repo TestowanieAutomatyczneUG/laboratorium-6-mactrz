b91197f9e97330735cb4e13ee906b9df5d6cec6a
Maciej_Trzasalski <mtrzsalski@gmail.com>
2020-11-09 17:52:39 +0100

    test7red

--- zad2/src/app.py
+++ zad2/src/app.py
@@ -1,15 +1,16 @@
 class Pass():
     def ValidPassword(self, passw):
+        """Check the given password
+        """
         letters = False
         special = False
         bigletter = False
         isnumber = False
         number = 0
         for i in passw:
+            number += 1
             if i.isupper():
                 bigletter = True
-            if i.isalpha():
-                number += 1
             if i.isnumeric():
                 isnumber = True
             if not i.isalnum():
--- zad2/tests/testsapp.py
+++ zad2/tests/testsapp.py
@@ -5,13 +5,23 @@ paass = app.Pass()
 class PasswordTest(unittest.TestCase):
     def test_1_true(self):
         self.assertEqual(paass.ValidPassword("Password1$"), True)
+
     def test_2_true(self):
         self.assertEqual(paass.ValidPassword("maPasswe2rd**"), True)
+
     def test_3_false_no_special(self):
         self.assertEqual(paass.ValidPassword("PppPaAcc2dasx"), False)
+
     def test_4_false_no_upper(self):
         self.assertEqual(paass.ValidPassword("%^*sdasfage2aegse"), False)
+
     def test_5_false_no_number(self):
         self.assertEqual(paass.ValidPassword("%^(macgeFEWdaf"), False)
+
     def test_6_false_short(self):
         self.assertEqual(paass.ValidPassword("%*4M"), False)
+
+    def test_exception_type(self):
+        self.assertRaises(TypeError, paass.ValidPassword(4))
+
+
