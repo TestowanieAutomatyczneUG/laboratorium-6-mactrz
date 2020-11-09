0b35ca3d123e065d3942f89cbabc7ce4288cd449
Maciej_Trzasalski <mtrzsalski@gmail.com>
2020-11-09 17:25:10 +0100

    test2red

--- zad2/tests/testsapp.py
+++ zad2/tests/testsapp.py
@@ -5,3 +5,5 @@ paass = app.Pass()
 class PasswordTest(unittest.TestCase):
     def test_1_true(self):
         self.assertEqual(paass.ValidPassword("Password1$"), True)
+    def test_2_true(self):
+        self.assertEqual(paass.ValidPassword("maPasswerd**"), True)
